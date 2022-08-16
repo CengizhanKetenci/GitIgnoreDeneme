4663b01620d0c1fbb65e897f40dca7a7a2ab6078
Cengizhan Ketenci <cengizhanketenci79@gmail.com>
2022-08-16 22:54:32 +0300

    aaa

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -3,5 +3,6 @@ public class GitIgnore {
     public static void main(String[] args) {
 
         System.out.println("GitIgnore deneme 1");
+        System.out.println("GitIgnore deneme Branch");
     }
 }
